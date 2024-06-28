d4964d761ae81d0e55696ac62b614ac790b074ab
MaheshBoo <136978753+MaheshBoo@users.noreply.github.com>
2024-06-28 15:25:08 +0530

    Add files via upload

---
+++ Music App/src/data/Audiodata.js
@@ -0,0 +1,17 @@
+const Audiodata = [
+    {
+        id: 1,
+        name: 'Song One',
+        img: "https://e-cdns-images.dzcdn.net/images/playlist/1e2a0058d6d373a687a6f4372a6e4932/500x500-000000-80-0-0.jpg",
+        audioSrc: 'https://wynk.in/music/song/audio/sm_A10328E0008693050H',
+        artist: 'Artist One'
+    },
+    {
+        id: 2,
+        name: 'Song Two',
+        img: "https://e-cdns-images.dzcdn.net/images/playlist/1e2a0058d6d373a687a6f4372a6e4932/500x500-000000-80-0-0.jpg",
+        audioSrc: 'https://wynk.in/music/song/audio/sm_A10328E0008693050H',
+        artist: 'Artist Two'
+      },
+]
+export {Audiodata};
